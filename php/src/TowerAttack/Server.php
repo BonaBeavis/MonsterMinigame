3a49df921f08d96793450ab82698bdd2fd63c9a0
Beavis <gordian.dziwis@gmail.com>
2016-06-22 12:07:35 +0200

    Formats Server.php

--- php/src/TowerAttack/Server.php
+++ php/src/TowerAttack/Server.php
@@ -3,277 +3,277 @@ namespace SteamDB\CTowerAttack;
 
 class Server
 {
-	public $SaneServer;
-	private $Running;
-	private $LastTick;
-	private $LastSecond;
-	private $Socket;
-	private $Game;
-	private static $Logger;
-	protected $TickRate;
-	protected static $TuningData = array();
+    public $SaneServer;
+    private $Running;
+    private $LastTick;
+    private $LastSecond;
+    private $Socket;
+    private $Game;
+    private static $Logger;
+    protected $TickRate;
+    protected static $TuningData = array();
 
-	public function __construct( $Port )
-	{
-		self::$Logger = new Logger;
-		$this->Socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP );
+    public function __construct( $Port )
+    {
+        self::$Logger = new Logger;
+        $this->Socket = socket_create( AF_INET, SOCK_STREAM, SOL_TCP );
 
-		socket_set_option( $this->Socket, SOL_SOCKET, SO_REUSEADDR, 1 );
-		socket_bind( $this->Socket, '0.0.0.0', $Port );
-		socket_listen( $this->Socket, 5 );
+        socket_set_option( $this->Socket, SOL_SOCKET, SO_REUSEADDR, 1 );
+        socket_bind( $this->Socket, '0.0.0.0', $Port );
+        socket_listen( $this->Socket, 5 );
 
-		self::GetLogger()->info( 'Server is listening on port ' . $Port );
+        self::GetLogger()->info( 'Server is listening on port ' . $Port );
 
-		self::LoadTuningData();
+        self::LoadTuningData();
 
-		$this->TickRate = self::GetTuningData( 'tick_rate' );
-		$this->Game = new Game( 44925 ); #TODO: dynamic room ids
-	}
+        $this->TickRate = self::GetTuningData( 'tick_rate' );
+        $this->Game = new Game( 44925 ); #TODO: dynamic room ids
+    }
 
-	public function Listen( )
-	{
-		$this->Running = true;
+    public function Listen( )
+    {
+        $this->Running = true;
 
-		while( $this->Running )
-		{
-			$Message = socket_accept( $this->Socket );
+        while( $this->Running )
+        {
+            $Message = socket_accept( $this->Socket );
 
-			$DebugTime = microtime( true );
+            $DebugTime = microtime( true );
 
-			$Data = socket_read( $Message, 2048 );
-			$Data = json_decode( $Data, TRUE );
+            $Data = socket_read( $Message, 2048 );
+            $Data = json_decode( $Data, TRUE );
 
-			if( !isset( $Data[ 'method' ] ) )
-			{
-				socket_shutdown( $Message, 2 );
-				socket_close( $Message );
+            if( !isset( $Data[ 'method' ] ) )
+            {
+                socket_shutdown( $Message, 2 );
+                socket_close( $Message );
 
-				// Require all data sent to the server to be a JSON object and contain the "method" key, ignore everything else.
-			    continue;
-			}
+                // Require all data sent to the server to be a JSON object and contain the "method" key, ignore everything else.
+                continue;
+            }
 
-			self::GetLogger()->debug( ( isset( $Data[ 'steamid' ] ) ? ( $Data[ 'steamid' ] . ' - ' ) : '' ) . $Data[ 'method' ] );
+            self::GetLogger()->debug( ( isset( $Data[ 'steamid' ] ) ? ( $Data[ 'steamid' ] . ' - ' ) : '' ) . $Data[ 'method' ] );
 
-			// Handle the request, this could be moved elsewhere...
-			$Response = [];
+            // Handle the request, this could be moved elsewhere...
+            $Response = [];
 
-			switch ( $Data[ 'method' ] )
-			{
-				// TODO: Remove this in favor of UseAbilities
-				case 'ChatMessage':
-					$Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
+            switch ( $Data[ 'method' ] )
+            {
+                // TODO: Remove this in favor of UseAbilities
+            case 'ChatMessage':
+                $Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
 
-					if( $Player === null )
-					{
-						break;
-					}
+                if( $Player === null )
+                {
+                    break;
+                }
 
-					$Response = true;
+                $Response = true;
 
-					$Player->HandleAbilityUsage( $this->Game, [ [ 'ability' => Enums\EAbility::ChatMessage, 'message' => mb_substr( $Data[ 'message' ], 0, 500 ) ] ] );
+                $Player->HandleAbilityUsage( $this->Game, [ [ 'ability' => Enums\EAbility::ChatMessage, 'message' => mb_substr( $Data[ 'message' ], 0, 500 ) ] ] );
 
-					break;
-				case 'GetGameData':
-					$Response =
-					[
-						'game_data' => $this->Game->ToArray()
-					];
+                break;
+            case 'GetGameData':
+                $Response =
+                    [
+                        'game_data' => $this->Game->ToArray()
+                    ];
 
-					if( $Data[ 'include_stats' ] )
-					{
-						$Response[ 'stats' ] = $this->Game->GetStats();
-					}
+                if( $Data[ 'include_stats' ] )
+                {
+                    $Response[ 'stats' ] = $this->Game->GetStats();
+                }
 
-					break;
-				case 'GetPlayerData':
-					$Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
+                break;
+            case 'GetPlayerData':
+                $Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
 
-					if( $Player === null )
-					{
-						// TODO: for now
-						$Player = $this->Game->CreatePlayer( $Data[ 'steamid' ], $Data[ 'player_name' ] );
-						//break;
-					}
+                if( $Player === null )
+                {
+                    // TODO: for now
+                    $Player = $this->Game->CreatePlayer( $Data[ 'steamid' ], $Data[ 'player_name' ] );
+                    //break;
+                }
 
-					$Response =
-					[
-						'player_data' => $Player->ToArray(),
-					];
+                $Response =
+                    [
+                        'player_data' => $Player->ToArray(),
+                    ];
 
-					if( $Data[ 'include_tech_tree' ] )
-					{
-						$Response[ 'tech_tree' ] = $Player->GetTechTree()->ToArray();
-					}
+                if( $Data[ 'include_tech_tree' ] )
+                {
+                    $Response[ 'tech_tree' ] = $Player->GetTechTree()->ToArray();
+                }
 
-					if( $Data[ 'include_stats' ] )
-					{
-						$Response[ 'stats' ] = $Player->Stats->ToArray();
-					}
+                if( $Data[ 'include_stats' ] )
+                {
+                    $Response[ 'stats' ] = $Player->Stats->ToArray();
+                }
 
-					break;
-				case 'UseBadgePoints':
-					$Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
+                break;
+            case 'UseBadgePoints':
+                $Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
 
-					if( $Player === null )
-					{
-						break;
-					}
+                if( $Player === null )
+                {
+                    break;
+                }
 
-					$Player->HandleBadgePoints( $this->Game, $Data[ 'ability_items' ] );
-					$this->Game->UpdatePlayer( $Player );
-					$Response =
-					[
-						'player_data' => $Player->ToArray(),
-						'tech_tree' => $Player->GetTechTree()->ToArray()
-					];
+                $Player->HandleBadgePoints( $this->Game, $Data[ 'ability_items' ] );
+                $this->Game->UpdatePlayer( $Player );
+                $Response =
+                    [
+                        'player_data' => $Player->ToArray(),
+                        'tech_tree' => $Player->GetTechTree()->ToArray()
+                    ];
 
-					break;
-				case 'ChooseUpgrade':
-					$Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
+                break;
+            case 'ChooseUpgrade':
+                $Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
 
-					if( $Player === null )
-					{
-						break;
-					}
+                if( $Player === null )
+                {
+                    break;
+                }
 
-					$Player->HandleUpgrade( $this->Game, $Data[ 'upgrades' ] );
-					$this->Game->UpdatePlayer( $Player );
-					$Response =
-					[
-						'tech_tree' => $Player->GetTechTree()->ToArray()
-					];
+                $Player->HandleUpgrade( $this->Game, $Data[ 'upgrades' ] );
+                $this->Game->UpdatePlayer( $Player );
+                $Response =
+                    [
+                        'tech_tree' => $Player->GetTechTree()->ToArray()
+                    ];
 
-					break;
-				case 'UseAbilities':
-					$Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
+                break;
+            case 'UseAbilities':
+                $Player = $this->Game->GetPlayer( $Data[ 'steamid' ] );
 
-					if( $Player === null )
-					{
-						break;
-					}
+                if( $Player === null )
+                {
+                    break;
+                }
 
-					$Player->HandleAbilityUsage( $this->Game, $Data[ 'requested_abilities' ] );
-					$this->Game->UpdatePlayer( $Player );
+                $Player->HandleAbilityUsage( $this->Game, $Data[ 'requested_abilities' ] );
+                $this->Game->UpdatePlayer( $Player );
 
-					$Response =
-					[
-						'player_data' => $Player->ToArray()
-					];
+                $Response =
+                    [
+                        'player_data' => $Player->ToArray()
+                    ];
 
-					foreach( $Data[ 'requested_abilities' ] as $RequestedAbility )
-					{
-						// We don't need to send techtree if user only sent clicks to target change
-						if( $RequestedAbility[ 'ability' ] !== Enums\EAbility::Attack
-						||  $RequestedAbility[ 'ability' ] !== Enums\EAbility::ChangeTarget )
-						{
-							$Response[ 'tech_tree' ] = $Player->GetTechTree()->ToArray();
-							break;
-						}
-					}
+                foreach( $Data[ 'requested_abilities' ] as $RequestedAbility )
+                {
+                    // We don't need to send techtree if user only sent clicks to target change
+                    if( $RequestedAbility[ 'ability' ] !== Enums\EAbility::Attack
+                        ||  $RequestedAbility[ 'ability' ] !== Enums\EAbility::ChangeTarget )
+                    {
+                        $Response[ 'tech_tree' ] = $Player->GetTechTree()->ToArray();
+                        break;
+                    }
+                }
 
-					break;
-				default:
-					// TODO: handle unknown methods
-					break;
-			}
+                break;
+            default:
+                // TODO: handle unknown methods
+                break;
+            }
 
-			$Response = json_encode( [ 'response' => $Response ], JSON_PRETTY_PRINT );
+            $Response = json_encode( [ 'response' => $Response ], JSON_PRETTY_PRINT );
 
-			socket_write( $Message, $Response );
-			socket_shutdown( $Message, 1 );
-			socket_close( $Message );
+            socket_write( $Message, $Response );
+            socket_shutdown( $Message, 1 );
+            socket_close( $Message );
 
-			$Tick = microtime( true );
+            $Tick = microtime( true );
 
-			if( $Tick >= $this->LastTick )
-			{
-				$this->LastTick = $Tick + $this->TickRate;
+            if( $Tick >= $this->LastTick )
+            {
+                $this->LastTick = $Tick + $this->TickRate;
 
-				if( $Tick >= $this->LastSecond )
-				{
-					$SecondsPassed = isset( $this->LastSecond ) ? floor( $Tick + 1.0 - $this->LastSecond ) : false;
+                if( $Tick >= $this->LastSecond )
+                {
+                    $SecondsPassed = isset( $this->LastSecond ) ? floor( $Tick + 1.0 - $this->LastSecond ) : false;
 
-					$this->LastSecond = $Tick + 1.0; // constant rate, does not change
+                    $this->LastSecond = $Tick + 1.0; // constant rate, does not change
 
-					$this->Tick( $Tick, $SecondsPassed );
-				}
-				else
-				{
-					$this->Tick( $Tick, false );
-				}
-				
-				$this->Game->TotalTicks++;
-			}
+                    $this->Tick( $Tick, $SecondsPassed );
+                }
+                else
+                {
+                    $this->Tick( $Tick, false );
+                }
 
-			$DebugTime = microtime( true ) - $DebugTime;
+                $this->Game->TotalTicks++;
+            }
 
-			self::GetLogger()->debug( 'Spent ' . number_format( $DebugTime, 7 ) . ' seconds handling sockets and ticks' );
+            $DebugTime = microtime( true ) - $DebugTime;
 
-			if( $DebugTime > $this->Game->HighestTick )
-			{
-				$this->Game->HighestTick = $DebugTime;
-			}
+            self::GetLogger()->debug( 'Spent ' . number_format( $DebugTime, 7 ) . ' seconds handling sockets and ticks' );
 
-			$this->Game->TimeSimulating += $DebugTime;
-		}
+            if( $DebugTime > $this->Game->HighestTick )
+            {
+                $this->Game->HighestTick = $DebugTime;
+            }
 
-		socket_shutdown( $this->Socket, 2 );
-		socket_close( $this->Socket );
+            $this->Game->TimeSimulating += $DebugTime;
+        }
 
-		self::GetLogger()->info( 'Sockets closed' );
-	}
+        socket_shutdown( $this->Socket, 2 );
+        socket_close( $this->Socket );
 
-	private function Tick( $Tick, $SecondsPassed )
-	{
-		self::GetLogger()->debug( 'Ticking... seconds passed: ' . $SecondsPassed );
+        self::GetLogger()->info( 'Sockets closed' );
+    }
 
-		if( $this->SaneServer )
-		{
-			pcntl_signal_dispatch();
-		}
+    private function Tick( $Tick, $SecondsPassed )
+    {
+        self::GetLogger()->debug( 'Ticking... seconds passed: ' . $SecondsPassed );
 
-		$this->Game->Update( $SecondsPassed );
-	}
+        if( $this->SaneServer )
+        {
+            pcntl_signal_dispatch();
+        }
 
-	public static function LoadTuningData()
-	{
-		$File = file_get_contents( __DIR__ . '/../../files/tuningData.json' );
+        $this->Game->Update( $SecondsPassed );
+    }
 
-		self::$TuningData = json_decode( $File, true );
+    public static function LoadTuningData()
+    {
+        $File = file_get_contents( __DIR__ . '/../../files/tuningData.json' );
 
-		if( empty( $File ) )
-		{
-			self::GetLogger()->error( 'Failed to load tuning data' );
+        self::$TuningData = json_decode( $File, true );
 
-			die;
-		}
-	}
+        if( empty( $File ) )
+        {
+            self::GetLogger()->error( 'Failed to load tuning data' );
 
-	public static function GetTuningData( $key = null )
-	{
-		if ( empty( self::$TuningData ) ) {
-			self::LoadTuningData();
-		}
-		return $key !== null ? self::$TuningData[$key] : self::$TuningData;
-	}
+            die;
+        }
+    }
 
-	public function GetGame()
-	{
-		return $this->Game;
-	}
+    public static function GetTuningData( $key = null )
+    {
+        if ( empty( self::$TuningData ) ) {
+            self::LoadTuningData();
+        }
+        return $key !== null ? self::$TuningData[$key] : self::$TuningData;
+    }
 
-	public function Shutdown()
-	{
-		if( $this->Running )
-		{
-			$this->Running = false;
+    public function GetGame()
+    {
+        return $this->Game;
+    }
 
-			self::GetLogger()->info( 'Good bye' );
-		}
-	}
+    public function Shutdown()
+    {
+        if( $this->Running )
+        {
+            $this->Running = false;
 
-	public static function GetLogger()
-	{
-		return self::$Logger;
-	}
+            self::GetLogger()->info( 'Good bye' );
+        }
+    }
+
+    public static function GetLogger()
+    {
+        return self::$Logger;
+    }
 }
